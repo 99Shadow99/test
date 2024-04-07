cbc82a1974fee0077becdfadd2ca2909c07eff33
99Shadow99 <140572212+99Shadow99@users.noreply.github.com>
2024-04-07 08:20:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+
+
+document.querySelector("#hi").addEventListener("mousemove",function(dets){
+    document.querySelector("#hi").style.left=dets.x-50+"px";
+    document.querySelector("#hi").style.top=dets.y-50+"px";
+  
+})
+
+apendchild
+setInterval
+set attrib
+removechild
+createlement
+childnodes
+getattribiue.addEventListener
